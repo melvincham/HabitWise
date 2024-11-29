746b716e065b5531213f5b011789b63c2af2594e
Melvin Cham Acho <achomelvincham@gmail.com>
2024-11-28 22:50:46 -0700

    Added a navigation service to facilitate Mocking

---
+++ HabitWise/Services/INavigationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitWise.Services
+{
+    public interface INavigationService
+    {
+        public void GoToAsync(string page, bool animate = true);
+    }
+}
---
+++ HabitWise/Services/NavigationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitWise.Services
+{
+    internal class NavigationService : INavigationService
+    {
+        public async void GoToAsync(string page, bool animate = true) 
+        {
+            await Shell.Current.GoToAsync(page, animate);
+        }
+    }
+}
