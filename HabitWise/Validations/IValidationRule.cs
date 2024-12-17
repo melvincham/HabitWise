12421813b5d0475fa62777a07e01799c835a605f
Melvin Cham Acho <achomelvincham@gmail.com>
2024-12-17 01:31:34 -0700

    Created a validation technique following maui interprise application patterns

---
+++ HabitWise/Validations/IValidationRule.cs
@@ -0,0 +1,8 @@
+﻿namespace HabitWise.Validations;
+
+public interface IValidationRule<T>
+{
+    string ValidationMessage { get; set; }
+
+    bool Check(T value);
+}
---
+++ HabitWise/Validations/PasswordRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HabitWise.Validations
+{
+    public class PasswordRule<T> : IValidationRule<T>
+    {
+        public string ValidationMessage { get; set; }
+
+        public bool Check(T value) {
+            return true;
+        }
+            
+    }
+}
